41e815ef7dbf5ef7d02c4252e9b1f6f039a78de9
Equinox-13 <quaid.johar33@gmail.com>
2021-07-11 20:06:04 +0530

    Add root path response

--- api/main.py
+++ api/main.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+	return {"greetings": "Hello world"}
